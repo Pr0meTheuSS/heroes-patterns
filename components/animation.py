7848b8a96acfd72ec2dada63484b6dd9427f87a2
yolimpiev <yr.olimpiev@gmail.com>
2025-06-25 15:47:02 +0700

    Implement base unit state logic

--- components/animation.py
+++ components/animation.py
@@ -1,21 +1,38 @@
 from pygame import Surface
 from dataclasses import dataclass
 
+
 @dataclass
 class Animation:
-    frames: list[Surface]
+    animations: dict[str, list[Surface]]
     frame_duration: float
     loop: bool = True
     current_time: float = 0.0
     current_frame: int = 0
-    # TODO: think about move behaivor to system ???
+    current_state: str = "idle"
+
     def update(self, dt: float):
         self.current_time += dt
+        frames = self.animations[self.current_state]
+
         if self.current_time >= self.frame_duration:
             self.current_time -= self.frame_duration
             self.current_frame += 1
-            if self.current_frame >= len(self.frames):
+
+            if self.current_frame >= len(frames):
                 if self.loop:
                     self.current_frame = 0
                 else:
-                    self.current_frame = len(self.frames) - 1
+                    self.current_frame = len(frames) - 1
+
+    def get_frames(self) -> list[Surface]:
+        return self.animations[self.current_state]
+
+    def get_current_frame(self) -> Surface:
+        return self.animations[self.current_state][self.current_frame]
+
+    def set_state(self, new_state: str):
+        if new_state != self.current_state:
+            self.current_state = new_state
+            self.current_frame = 0
+            self.current_time = 0.0
--- systems/movement_system.py
+++ systems/movement_system.py
@@ -1,4 +1,4 @@
-from components import HexPosition, Path
+from components import HexPosition, Path, Animation
 
 
 def movement_system(ecs, dt):
@@ -7,6 +7,7 @@ def movement_system(ecs, dt):
         path = ecs.get(Path, entity)
 
         if path.current_index < len(path.steps):
+            ecs.get(Animation, entity).set_state("walk")
             path.step_timer -= dt
             if path.step_timer <= 0:
                 target_q, target_r = path.steps[path.current_index]
@@ -14,4 +15,6 @@ def movement_system(ecs, dt):
                 path.current_index += 1
                 path.step_timer = path.step_delay
         else:
+            print("change state to idle")
+            ecs.get(Animation, entity).set_state("idle")
             ecs.remove_component(entity, Path)
