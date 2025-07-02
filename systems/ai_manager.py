36fbcd71dd114ce955dbcdaf9887541824901839
yolimpiev <yr.olimpiev@gmail.com>
2025-07-03 00:09:30 +0700

    Fix trouble with turn move from ai to player

--- systems/ai_manager.py
+++ systems/ai_manager.py
@@ -3,6 +3,8 @@ from components import Team
 from components import HexPosition
 from components import Initiative
 from components import Path
+from commands import AttackCommand
+from components import Animation
 from pathfinding import bfs_with_fallback
 from pathfinding import is_passable
 
@@ -10,18 +12,30 @@ from pathfinding import is_passable
 def ai_managment(ecs, turn_manager):
     for entity in ecs.get_entities_with(AiManagable):
         active = turn_manager.get_active_unit()
+        path = ecs.get(Path, active)
         if active and ecs.get(Team, active).name != "player":
-            print("Handle ai managnent for entity", entity)
-            start = ecs.get(HexPosition, active)
-            initiative = ecs.get(Initiative, active)
-            path = bfs_with_fallback(
-                (start.q, start.r),
-                (5, 5),
-                lambda q_, r_: is_passable(q_, r_, ecs),
-            )[: initiative.value + 1]
-            if path:
-                ecs.add_component(active, Path(path[1:]))
+            if (
+                path
+                and path.current_index >= len(path.steps)
+                and not ecs.get(AttackCommand, active)
+            ):
+                ecs.components[Path].pop(active, None)
+                print("end turn")
+                ecs.get(Animation, active).set_state("idle")
+                turn_manager.end_turn()
+                return
+            if path is None:
+                start = ecs.get(HexPosition, active)
+                initiative = ecs.get(Initiative, active)
+                path = bfs_with_fallback(
+                    (start.q, start.r),
+                    (5, 5),
+                    lambda q_, r_: is_passable(q_, r_, ecs),
+                )[: initiative.value + 1]
+                if path:
+                    ecs.add_component(active, Path(path[1:]))
+                path = ecs.get(Path, active)
+                print(path.current_index, len(path.steps))
 
-            turn_manager.end_turn()
         # TODO: make decidion
         # Add turn to queue
