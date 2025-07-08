bd33477c2626585a5a6481a9233e6287aa1c64f6
yolimpiev <yr.olimpiev@gmail.com>
2025-07-09 01:51:38 +0700

    Implement idle, walk, hurt, attack and dead animation
    Some animations are looping (walk, idle)
    Check state and display state animation.
    
    Need fix for fast switching between "hurt" and "walk" states
    and animations.

--- components/animation.py
+++ components/animation.py
@@ -1,15 +1,17 @@
 from pygame import Surface
 from dataclasses import dataclass
 
+states = ["idle", "walk", "attack", "hurt", "dead"]
+
 
 @dataclass
 class Animation:
     animations: dict[str, list[Surface]]
     frame_duration: float
-    loop: bool = True
     current_time: float = 0.0
     current_frame: int = 0
     current_state: str = "idle"
+    queued_state: str = ""
 
     def update(self, dt: float):
         self.current_time += dt
@@ -20,10 +22,16 @@ class Animation:
             self.current_frame += 1
 
             if self.current_frame >= len(frames):
-                if self.loop:
+                if self.is_looping():
                     self.current_frame = 0
                 else:
                     self.current_frame = len(frames) - 1
+                    if self.current_state != "dead":
+                        self.set_state("idle")
+
+        if self.queued_state != "":
+            self.current_state = self.queued_state
+            self.queued_state = ""
 
     def get_frames(self) -> list[Surface]:
         return self.animations[self.current_state]
@@ -32,7 +40,14 @@ class Animation:
         return self.animations[self.current_state][self.current_frame]
 
     def set_state(self, new_state: str):
+        frames = self.animations[self.current_state]
+
+        if self.current_frame != len(frames) - 1:
+            self.queued_state = new_state
         if new_state != self.current_state:
             self.current_state = new_state
             self.current_frame = 0
             self.current_time = 0.0
+
+    def is_looping(self):
+        return self.current_state in ["walk", "idle"]
